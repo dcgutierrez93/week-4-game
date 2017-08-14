a8c8130602fa80c9f803c24edda2668c92e775ef
David Gutierrez <davidgutierrez@Davids-iMac.local>
2017-08-14 08:58:28 -0500

    Added necasary index.html

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+var chosenCharacter;
+var chosenEnemy;
+var isEnemyChosen = false;
+var isCharacterChosen = false;
+var isGameOver = false;
+var hasUserLost = false;
+// Character Objects.
+var luke-sky = {
+  healthPoints: 120,
+  attackPower: 8,
+  counterAttackPwr: 5
+};
+
+var darth-vadar = {
+  healthPoints: 150,
+  attackPower: 5,
+  counterAttackPwr: 4
+};
+
+var yoda = {
+  healthPoints: 130,
+  attackPower: 7
+  counterAttackPwr: 6
+};
+
+var darth-maul = {
+  healthPoints: 140,
+  attackPower: 6,
+  counterAttackPwr: 5
+};
+
+var enemeies = [];
+var defeated = [];
+
+$(document).ready(function(){
+  
+});
